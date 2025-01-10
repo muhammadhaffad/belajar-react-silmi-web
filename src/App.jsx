9b6c55edec897a3ccde3301de7956c860647aa95
hadap <robottxt5@gmail.com>
2025-01-10 16:13:59 +0700

    TODO: state filter

--- src/App.jsx
+++ src/App.jsx
@@ -67,24 +67,29 @@ const sliderImages = [
   'http://silmiofficial.com/assets/images/banners/CLEREANCE%20SALE.jpg'
 ]
 const colors = [
-  { code: 'PI', name: 'Pink' },
-  { code: 'HT', name: 'Hitam' },
-  { code: 'MR', name: 'Maroon' },
-  { code: 'ME', name: 'Merah' },
-  { code: 'PB', name: 'Putih Bersih' },
-  { code: 'PT', name: 'Putih Tulang' },
-  { code: 'HJ', name: 'Hijau' },
-];
+  { value: 'PI', label: 'Pink' },
+  { value: 'HT', label: 'Hitam' },
+  { value: 'MR', label: 'Maroon' },
+  { value: 'ME', label: 'Merah' },
+  { value: 'PB', label: 'Putih Bersih' },
+  { value: 'PT', label: 'Putih Tulang' },
+  { value: 'HJ', label: 'Hijau' },
+].sort((a, b) => a.label.localeCompare(b.label));
 const sizes = [
   '02', '04', '06', '08', '10', '12', '14', 'XS', 'S', 'M', 'L', 'XL', 'XXL', '3XL', '4XL'
 ]
-const sorting = [
-  { sortCode: '', name: 'Paling Sesuai' },
-  { sortCode: '3', name: 'Rilis: Baru ke Lama' },
-  { sortCode: '4', name: 'Rilis: Lama ke Baru' },
-  { sortCode: '1', name: 'Harga: Rendah ke Tinggi' },
-  { sortCode: '2', name: 'Harga: Tinggi ke Rendah' },
-]
+const sortOptions = [
+  { value: '', label: 'Paling Sesuai' },
+  { value: '3', label: 'Rilis: Baru ke Lama' },
+  { value: '4', label: 'Rilis: Lama ke Baru' },
+  { value: '1', label: 'Harga: Rendah ke Tinggi' },
+  { value: '2', label: 'Harga: Tinggi ke Rendah' },
+];
+
+const min = 0;
+const max = 500000;
+const step = 10000;
+
 const BannerCarousel = () => {
   return (
     <div>
@@ -105,32 +110,40 @@ const BannerCarousel = () => {
   )
 }
 function App() {
-  const [values, setValues] = useState([100000, 400000]);
   const [searchColor, setSerachColor] = useState('');
-  const [sortBy, setSortBy] = useState(sorting[0]);
   const [filterActive, setFilterActive] = useState(0);
   const [chooseFilter, setChooseFilter] = useState(null);
   const [filter, setFilter] = useState({
     harga: [100000,400000],
-    warna: [],
-    ukuran: []
+    warna: colors.map((color) => ({...color, ['checked']: false})),
+    ukuran: sizes.map(item => Object.assign({}, {value: item, label: item, checked: false}))
   });
   const [filterControl, setFilterControl] = useState({
-    sortBy: ''
+    sortBy: sortOptions[0].value
   });
   const filterRefs = useRef([]);
-  const min = 0;
-  const max = 500000;
-  const step = 10000;
 
-  colors.sort((a, b) => a.name.localeCompare(b.name));
-  const filteredColors = colors.filter(item => item.name.toLowerCase().includes(searchColor.toLowerCase()));
+  const filteredColors = filter.warna.filter(
+    color => color.label.toLowerCase().includes(searchColor.toLowerCase())
+  );
+
   const handleClickButtonFilter = (index) => {
     if (index === filterActive) {
       setFilterActive(0);
     } else {
       setFilterActive(index);
     }
+  }
+  const handleClickColorFilter = (value, checked) => {
+    setFilter({...filter, ['warna']: filter.warna.map(
+      (o) => {
+        if(o.value == value) {
+          return ({...o, ['checked']: !checked})
+        } else {
+          return ({...o});
+        }
+      }
+    )})
   }
   const handleClickSortBy = (sortCode) => {
     setFilterControl({...filterControl, ['sortBy']: sortCode});
@@ -164,13 +177,18 @@ function App() {
             <div className={`filter__panel ${chooseFilter === 'panel' ? 'active' : ''}`}>
               <div className='filter__header p-3 border-bottom text-uppercase lh-1 sticky-top top-0 bg-white d-flex align-items-center justify-content-between d-lg-none'><span>Terapkan Filter</span><button onClick={() => setChooseFilter(null)} className='btn p-0'><i className="fs-5 bi bi-x-lg"></i></button></div>
               <div ref={(el) => filterRefs.current[0] = el} className="filter__panel-item">
-                <button onClick={() => handleClickButtonFilter(1)} className={`filter__button btn ${filterActive === 1 ? 'active' : ''}`}>Harga<span>{filterActive === 1 ? '−' : '+'}</span></button>
+                <button onClick={() => handleClickButtonFilter(1)} className={`filter__button btn ${filterActive === 1 ? 'active' : ''}`}>
+                  <div>
+                    Harga
+                    <small className='filter__status--mobile'>
+                      Rp{filter.harga[0].toLocaleString('id-ID', { minimumFractionDigits: 0, maximumFractionDigits: 2 })} − Rp{filter.harga[1].toLocaleString('id-ID', { minimumFractionDigits: 0, maximumFractionDigits: 2 })}
+                    </small>
+                  </div><span>{filterActive === 1 ? '−' : '+'}</span>
+                </button>
                 <div className="filter__options">
                   <div className="filter__options-container">
                     <div className="d-flex w-100 gap-1 justify-content-center">
-                      <span>Rp{filter.harga[0].toLocaleString('id-ID', { minimumFractionDigits: 0, maximumFractionDigits: 2 })}</span>
-                      −
-                      <span>Rp{filter.harga[1].toLocaleString('id-ID', { minimumFractionDigits: 0, maximumFractionDigits: 2 })}</span>
+                      <span>Rp{filter.harga[0].toLocaleString('id-ID', { minimumFractionDigits: 0, maximumFractionDigits: 2 })} − Rp{filter.harga[1].toLocaleString('id-ID', { minimumFractionDigits: 0, maximumFractionDigits: 2 })}</span>
                     </div>
                     <Range
                       values={filter.harga}
@@ -224,7 +242,15 @@ function App() {
                 </div>
               </div>
               <div ref={(el) => filterRefs.current[1] = el} className="filter__panel-item">
-                <button onClick={() => handleClickButtonFilter(2)} className={`filter__button btn ${filterActive === 2 ? 'active' : ''}`}>Warna<span>{filterActive === 2 ? '−' : '+'}</span></button>
+                <button onClick={() => handleClickButtonFilter(2)} className={`filter__button btn ${filterActive === 2 ? 'active' : ''}`}>
+                  <div>
+                    Warna
+                    <small className='filter__status--mobile'>
+                      {filter.warna.filter(o => o.checked).map(o => o.label).join(', ')}
+                    </small>
+                  </div>
+                  <span>{filterActive === 2 ? '−' : '+'}</span>
+                </button>
                 <div className="filter__options">
                   <div className="filter__options-container">
                     <div className="mb-3 border-bottom border-dark p-1 d-flex gap-2">
@@ -235,9 +261,9 @@ function App() {
                     </div>
                     <ul className='list-unstyled mb-0 d-flex flex-column flex-grow-1 gap-3 overflow-auto'>
                       {filteredColors.map(color => (
-                        <li key={color.code} className='d-flex gap-2'>
-                          <input type="checkbox" className='border border-dark form-check-input rounded-0 m-0' style={{ width: '1.5rem', height: '1.5rem' }} id={color.code} />
-                          <label htmlFor={color.code}>{color.name}</label>
+                        <li key={color.value} className='d-flex gap-2 w-100'>
+                          <input type="checkbox" checked={color.checked} onChange={() => handleClickColorFilter(color.value, color.checked)} className='border border-dark form-check-input rounded-0 m-0 prevent-select' style={{ width: '1.5rem', height: '1.5rem' }} id={color.value} />
+                          <label className='w-100 cursor-pointer' htmlFor={color.value}>{color.label}</label>
                         </li>
                       ))}
                     </ul>
@@ -245,14 +271,20 @@ function App() {
                 </div>
               </div>
               <div ref={(el) => filterRefs.current[2] = el} className="filter__panel-item">
-                <button onClick={() => handleClickButtonFilter(3)} className={`filter__button btn ${filterActive === 3 ? 'active' : ''}`}>Ukuran<span>{filterActive === 3 ? '−' : '+'}</span></button>
+                <button onClick={() => handleClickButtonFilter(3)} className={`filter__button btn ${filterActive === 3 ? 'active' : ''}`}>
+                  <div>
+                    Ukuran
+                    <small className='filter__status--mobile'>asdasd</small>
+                  </div>
+                  <span>{filterActive === 3 ? '−' : '+'}</span>
+                </button>
                 <div className="filter__options">
                   <div className="filter__options-container">
                     <div className="flex-grow-1 gap-2 overflow-auto d-grid grid-cols-4 grid-lg-cols-3">
-                      {sizes.map((size) => (
-                        <div key={size} className="form-group d-inline-block m-0 form-check-bg">
-                          <input type="checkbox" className="form-check-bg-input" id={size} autoComplete="false" />
-                          <label className="form-check-label text-center w-100 prevent-select rounded-0 border-dark" htmlFor={size}>{size}</label>
+                      {filter.ukuran.map((size) => (
+                        <div key={size.value} className="form-group d-inline-block m-0 form-check-bg">
+                          <input type="checkbox" checked={size.checked} className="form-check-bg-input" id={size.value} autoComplete="false" />
+                          <label className="form-check-label text-center w-100 prevent-select rounded-0 border-dark" htmlFor={size.value}>{size.label}</label>
                         </div>
                       ))}
                     </div>
@@ -261,14 +293,19 @@ function App() {
               </div>
             </div>
             <div className={`filter__controls ${chooseFilter === 'control' ? 'active' : ''}`}>
-              <div className='filter__header p-3 border-bottom text-uppercase lh-1 sticky-top top-0 bg-white d-flex align-items-center justify-content-between d-lg-none'><span>Urutkan Berdasarkan</span><button onClick={() => setChooseFilter(null)} className='btn p-0'><i className="fs-5 bi bi-x-lg"></i></button></div>
+              <div className='filter__header p-3 border-bottom text-uppercase lh-1 sticky-top top-0 bg-white d-flex align-items-center justify-content-between d-lg-none'>
+                <span>Urutkan Berdasarkan</span>
+                <button onClick={() => setChooseFilter(null)} className='btn p-0'>
+                  <i className="fs-5 bi bi-x-lg"></i>
+                </button>
+              </div>
               <div ref={(el) => filterRefs.current[3] = el} className="filter__controls-item">
-                <button onClick={() => handleClickButtonFilter(4)} className={`filter__button filter__button-sort btn ${filterActive === 4 ? 'active' : ''}`}>{sorting.find((item) => item.sortCode === filterControl.sortBy).name}<span>{filterActive === 4 ? '−' : '+'}</span></button>
+                <button onClick={() => handleClickButtonFilter(4)} className={`filter__button filter__button-sort btn ${filterActive === 4 ? 'active' : ''}`}>{sortOptions.find((option) => option.value === filterControl.sortBy).label}<span>{filterActive === 4 ? '−' : '+'}</span></button>
                 <div className="filter__options filter__options--right">
                   <div className="filter__options-container">
                     <ul className="list-unstyled d-flex flex-column gap-lg-3 mb-0 m-n3 mx-lg-n3 my-lg-0">
-                      {sorting.map((sort) => (
-                        <li key={sort.sortCode} onClick={() => handleClickSortBy(sort.sortCode)} className={`cursor-pointer text-lg- px-3 py-3 py-lg-0 lh-1 border-light border-bottom border-lg-0 ${sort.name === sorting.find((item) => item.sortCode === filterControl.sortBy).name ? 'fw-bold' : ''}`}>{sort.name}</li>
+                      {sortOptions.map((option) => (
+                        <li key={option.label} onClick={() => handleClickSortBy(option.value)} className={`cursor-pointer text-lg- px-3 py-3 py-lg-0 lh-1 border-light border-bottom border-lg-0 ${option.value === filterControl.sortBy ? 'fw-bold' : ''}`}>{option.label}</li>
                       ))}
                     </ul>
                   </div>
